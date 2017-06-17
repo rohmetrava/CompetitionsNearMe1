285baccf4cbb5e91188f4d6fa9fa206616be6a4d
Clarissa Xu <clarissa_xu24@outlook.com>
2017-06-17 17:52:25 -0400

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(".title-rename-form1").on("submit", function(evt) {
+	evt.preventDefault()
+	
+	var newTitle = $(".title-rename").val()
+	$(".title-1").text(newTitle)
+	//alert(newTitle)
+
+})
+$(".title-rename-form2").on("submit", function(evt) {
+	evt.preventDefault()
+	
+	var newTitle = $(".title-rename").val()
+	$(".title-2").text(newTitle)
+	//alert(newTitle)
+
+})
+$(".title-rename-form3").on("submit", function(evt) {
+	evt.preventDefault()
+	
+	var newTitle = $(".title-rename").val()
+	$(".title-3").text(newTitle)
+	//alert(newTitle)
+
+})
